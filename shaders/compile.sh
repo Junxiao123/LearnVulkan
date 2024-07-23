a0a502cf1415845e91da1053fe4b250c4666df96
jun xiao <jun.xiao@deepmirror.com>
2024-07-23 16:52:53 +0800

    Shader modules

---
+++ shaders/compile.sh
@@ -0,0 +1,2 @@
+/home/xiaojun/work/vulkan/vulkansdk-linux-x86_64-1.3.290.0/1.3.290.0/x86_64/bin/glslc 09_shader_base.vert -o 09_shader_base_vert.spv
+/home/xiaojun/work/vulkan/vulkansdk-linux-x86_64-1.3.290.0/1.3.290.0/x86_64/bin/glslc 09_shader_base.frag -o 09_shader_base_frag.spv
